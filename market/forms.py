3033bb8806b210aced23d2a1c82e612e84901312
Pablo DE <diazechevestepablo@gmail.com>
2021-12-21 17:05:25 -0300

    Validaciones y autentificacion

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,41 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, EqualTo, Email, DataRequired
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from market.models import User
+
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='Nombre: ', validators=[Length(min=4, max=12), DataRequired()])
-    email = StringField(label='Email: ', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label = 'Contraseña: ', validators=[Length(min=6), DataRequired()])
-    password2 = PasswordField(label = 'Confirmar contraseña: ', validators=[EqualTo('password1'), DataRequired()])
-    submit = SubmitField(label='Enviar')
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError(
+                'Ya existe un usuario con ese nombre, intente con otro')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(
+            email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError(
+                'Ya existe un usuario registrado con esa direccion de correo.')
+
+    username = StringField(label='Nombre: ',
+                           validators=[Length(min=4, max=12),
+                                       DataRequired()])
+
+    email_address = StringField(label='Email: ',
+                                validators=[Email(), DataRequired()])
+
+    password1 = PasswordField(label='Contraseña: ',
+                              validators=[Length(min=6),
+                                          DataRequired()])
+    password2 = PasswordField(
+        label='Confirmar contraseña: ',
+        validators=[EqualTo('password1'), DataRequired()])
+
+    submit = SubmitField(label='Crear cuenta')
+    
+    
+class LoginForm(FlaskForm):
+    username = StringField(label='Usuario', validators=[DataRequired()])
+    password = StringField(label='Contraseña', validators=[DataRequired()])
+    submit = SubmitField(label='Ingresar')
--- market/routes.py
+++ market/routes.py
@@ -1,7 +1,8 @@
+from flask_wtf import form
 from market import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-from market.forms import RegisterForm
+from market.forms import RegisterForm, LoginForm
 from market import db
 
 
@@ -22,14 +23,19 @@ def register_page():
     form = RegisterForm()
     if form.validate_on_submit():
         user_to_create = User(username=form.username.data,
-                              email_address=form.email.data,
-                              password_hash=form.password1.data)
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
         return redirect(url_for('market_page'))
 
     if form.errors != {}:
         for err_msg in form.errors.values():
-            print(f'Error creando el usuario: {err_msg}')
+            flash(f'Error creando el usuario: {err_msg}', category='danger')
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    return render_template('login.html', form=form)
