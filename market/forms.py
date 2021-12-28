3e3ca587988a8e422f62cb89d612ee21f158c456
Pablo DE <diazechevestepablo@gmail.com>
2021-12-28 16:26:11 -0300

    Comprar item

--- market/forms.py
+++ market/forms.py
@@ -39,3 +39,9 @@ class LoginForm(FlaskForm):
     username = StringField(label='Usuario', validators=[DataRequired()])
     password = PasswordField(label='Contraseña', validators=[DataRequired()])
     submit = SubmitField(label='Ingresar')
+    
+class PurchaseItemForm(FlaskForm):
+    submit = SubmitField(label='Comprar') 
+    
+class SellItemForm(FlaskForm):
+    submit = SubmitField(label='Vender') 
--- market/models.py
+++ market/models.py
@@ -3,9 +3,12 @@ from market import db, login_manager
 from market import bcrypt
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -21,7 +24,7 @@ class User(db.Model, UserMixin):
         if len(str(self.budget)) >= 4:
             return f'${str(self.budget)[:-3]}.{str(self.budget)[-3:]}'
         else:
-            return f"${self.budget}"  
+            return f"${self.budget}"
 
     @property
     def password(self):
@@ -31,10 +34,15 @@ class User(db.Model, UserMixin):
     def password(self, plain_text_password):
         self.password_hash = bcrypt.generate_password_hash(
             plain_text_password).decode('utf-8')
-    
+
     def check_password_correction(self, attempted_password):
-        return bcrypt.check_password_hash(self.password_hash, attempted_password)
-    
+        return bcrypt.check_password_hash(self.password_hash,
+                                          attempted_password)
+
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -47,3 +55,8 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+    
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
